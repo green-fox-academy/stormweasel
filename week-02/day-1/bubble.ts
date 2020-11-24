826cdf0d25584a153d4dbf5bbd83bb7b2acee7ea
stormweasel <silvercross@freemail.hu>
2020-11-24 14:08:06 +0100

    The ascending part is done, boolean parameter is pending

---
+++ week-02/day-1/bubble.ts
@@ -0,0 +1,35 @@
+'use script';
+
+//  Create a function that takes a list of numbers as parameter
+//  Returns a list where the elements are sorted in ascending numerical order
+//  Make a second boolean parameter, if it's `true` sort that list descending
+
+
+/*/  Example:
+console.log(bubble([34, 12, 24, 9, 5]));
+//  should print [5, 9, 12, 24, 34]
+console.log(advancedBubble([34, 12, 24, 9, 5], true));
+//  should print [34, 24, 12, 9, 5]´*/
+
+let bubble: number[] = [34, 12, 24, 9, 5];
+let advancedBubble: any [] = [[34, 12, 24, 9, 5], [true]];
+
+bubble.sort(function (a: number, b: number): number {
+  /*if (c) {
+    return b - a
+  } else */
+    return a-b;
+  }
+)
+console.log(bubble);
+
+
+/*
+function printParams(...arg): void {
+  for (let i = 0; i< arg.length; i++) {
+      console.log(arguments[i]);
+  }
+}
+
+printParams (45, 454, true, 'Feri');
+*/
