5d44faf438a000a6a0866e3bcc7970c1e1628320
stormweasel <silvercross@freemail.hu>
2020-12-06 15:33:25 +0100

    This has been left out from Style-check

--- week-01/day-4/parametric-average.ts
+++ week-01/day-4/parametric-average.ts
@@ -1,16 +1,16 @@
 'use strict';
-export{};
+export { };
 // Write a program that calculates the sum and the average from 1 to a given number.
 // Example input: 5
 // Example output: Sum: 15, Average: 3
 
 let number: number = 10;
-let sum: number =  0;
+let sum: number = 0;
 let average: number;
 
-for (let i = 1 ; i <= number; i++) {
-    sum = sum+i;
-    average = sum/i;
-} 
-  
-console.log('Sum:' + sum +', Average:' + average);
+for (let i = 1; i <= number; i++) {
+  sum = sum + i;
+  average = sum / i;
+}
+
+console.log('Sum:' + sum + ', Average:' + average);
