c0209290eb7ed8bc45e422959cec96b202b5adc6
stormweasel <silvercross@freemail.hu>
2021-01-01 23:45:26 +0100

    BattleApp added. Ship Testfile separated

---
+++ week-03/day-3/pirate/battle.app.ts
@@ -0,0 +1,27 @@
+'use stict';
+
+import { Ship } from './ship';
+
+class BattleApp {
+	protected _ship1: Ship;
+	protected _ship2: Ship;
+
+	constructor(name1?: string, name2?: string) {
+		this._ship1 = new Ship(name1) ?? new Ship();
+		this._ship2 = new Ship(name2) ?? new Ship();
+	}
+
+	public run(): void {
+		this._ship1.fillShip();
+		this._ship2.fillShip();
+
+		console.log(`Status of the ships:\n`);
+		this._ship1.shipStatus()
+		this._ship2.shipStatus()
+
+		this._ship1.battle(this._ship2, 1);
+	}
+}
+
+let battle = new BattleApp();
+battle.run();
