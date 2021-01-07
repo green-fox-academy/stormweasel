c20ba65b04e44289973719352729285f5a2d2e91
stormweasel <silvercross@freemail.hu>
2021-01-07 23:30:22 +0100

    Todos done

---
+++ week-06/day-4/todos/server.js
@@ -0,0 +1,13 @@
+' use strict';
+
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+const todos = ['get up', 'survive', 'go back to bed'];
+
+app.get('/', (req, res) => {
+res.render('home', {todos});
+});
+
+app.listen(3000);
