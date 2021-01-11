7dda4d705906767cf89ad3e372c97a903f45bae1
stormweasel <silvercross@freemail.hu>
2021-01-11 17:56:12 +0100

    Tests for 2 digit numbers written. The code has a flaw.

--- week-07/number-converter.test.js
+++ week-07/number-converter.test.js
@@ -26,3 +26,42 @@ test('Should return error massage, if amount is not a number', t => {
 	t.equal (actual, expected);
 	t.end();
 });
+
+// test('Should return the digits of amount ', t => {
+// 	const actual = numberConverter( 745, '$');
+// 	const expected = ['7','4','5'];
+
+// 	t.deepEqual (actual, expected);
+// 	t.end();
+// });
+// test('Should return the digits of amount as text', t => {
+// 	const actual = numberConverter( 745, '$');
+// 	const expected = 'sevenfourfive';
+
+// 	t.deepEqual (actual, expected);
+// 	t.end();
+// });
+
+test('Should return the one digit numbers as text', t => {
+	const actual = numberConverter( 8, '$');
+	const expected = 'eight';
+
+	t.deepEqual (actual, expected);
+	t.end();
+});
+
+test('Should return the ten numbers as text', t => {
+	const actual = numberConverter( 18, '$');
+	const expected = 'eightteen';
+
+	t.deepEqual (actual, expected);
+	t.end();
+});
+
+test('Should return the two digit numbers as text', t => {
+	const actual = numberConverter( 68, '$');
+	const expected = 'sixtyeight';
+
+	t.deepEqual (actual, expected);
+	t.end();
+});
