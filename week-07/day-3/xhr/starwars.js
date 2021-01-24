39c1cfc95802685dd94c0fd0da5b2b54253ad0ee
stormweasel <silvercross@freemail.hu>
2021-01-24 19:55:31 +0100

    star wars is done

--- week-07/day-3/xhr/starwars.js
+++ week-07/day-3/xhr/starwars.js
@@ -13,71 +13,123 @@
 // In the second list, display the movie like this: Movie title (release date).
 // Explore the documentation and find requited API endpoints by yourself.
 
+
 let search = document.querySelector('button[type="submit"]');
 let input = document.querySelector('input');
+let warning = document.querySelector('#warning-field');
 
+let buttons = document.querySelector('#buttons');
 let list = document.querySelector('.people');
-
 let filmsButton = document.querySelector('#films');
 let moviesList = document.querySelector('.movies');
 
-
-// function getApi(info) {
-// 	let request = new XMLHttpRequest();
-// 	request.open('GET', `${info}`);
-// 	request.send();
-// 	let field = parseRequest(request);
-// 	return field;
-// }
-
-// function parseRequest(request) {
-// 	request.onload = () => {
-// 		let field = JSON.parse(request.response).name;
-// 		console.log(field);
-// 		return field;
-// 	}
-// }
-
 search.addEventListener('click', () => {
-	console.log(input.value);
+	search.disabled = true;
+	filmsButton.disabled = false;
+
+	let restartButton = document.createElement('button');
+	restartButton.innerText = 'New search';
+	restartButton.className = "restart";
+	buttons.appendChild(restartButton);
+	restartButton.onclick = () => { location.reload(); };
+
 	if (input.value == "") {
 		console.log('nothing was given');
-		let nothing = document.createElement('p');
-		nothing.textContent = "There was nothing given in the input field"; 
-		document.body.insertBefore(nothing, list);
+		warning.textContent = "There was nothing given in the input field";
 	} else {
-		const xhr = new XMLHttpRequest();
-		xhr.open('GET', `https://swapi.dev/api/people/?search=${input.value}`)
-		xhr.send();
+		const character = new XMLHttpRequest();
+		character.open('GET', `https://swapi.dev/api/people/?search=${input.value}`);
+		character.send();
 
-		xhr.onreadystatechange = () => {
-			// function handleReadyStateChange () {
-			if (xhr.readyState === XMLHttpRequest.DONE) {
-				console.log(xhr.response);
+		character.onreadystatechange = () => {
+			if (character.readyState === XMLHttpRequest.DONE) {
 				let emptyAnswer = `{"count":0,"next":null,"previous":null,"results":[]}`;
-				if (xhr.response === emptyAnswer ) {
-					console.log(`There's no such character in the database`);
+				if (character.response === emptyAnswer) {
+					warning.textContent = `There's no such character in the database`;
 				} else {
-					let values = Object.values(JSON.parse(xhr.response).results[0]);
-					let keys = Object.keys(JSON.parse(xhr.response).results[0]);
-					console.log(values);
-					console.log(values[9]);
+					let values = Object.values(JSON.parse(character.response).results[0]);
+					let keys = Object.keys(JSON.parse(character.response).results[0]);
 					console.log(keys);
+					console.log(values);
 
-					// function makeRow(key, value) {
-					// 	let li = document.createElement('li');
-					// 	li.textContent = `${key}: ${value}`;
-					// 	list.appendChild(li);
-					// }
-
-
-					for (let i = 0; i < 8; i++) {
+					for (let i = 0; i < 13; i++) {
 						let li = document.createElement('li');
-						li.textContent = `${keys[i]}: ${values[i]}`;
+						if (i === 8) {
+							const home = new XMLHttpRequest();
+							home.open('GET', `${values[8]}`);
+							home.send();
+
+							home.onload = () => {
+								let planet = JSON.parse(home.response).name;
+								li.textContent = `${keys[i]}: ${planet}`;
+							}
+						} else if (i === 9) {
+							continue;
+						} else if (i === 10) {
+							if (values[i].length === 0) {
+								li.textContent = `${keys[i]}: n/a`;
+								list.appendChild(li);
+								continue;
+							}
+							const species = new XMLHttpRequest();
+							species.open('GET', `${values[i]}`);
+							species.send();
+							species.onload = () => {
+								let race = JSON.parse(species.response).name;
+								li.textContent = `${keys[i]}: ${race}`;
+							}
+						} else if (i === 11) {
+							if (values[i].length === 0) {
+								li.textContent = `${keys[i]}: n/a`;
+								list.appendChild(li);
+								continue;
+							}
+							let vehicles = [];
+							for (let j = 0; j < values[11].length; j++) {
+								const kind = new XMLHttpRequest();
+								kind.open('GET', `${values[11][j]}`);
+								kind.send();
+								kind.onload = () => {
+									let race = JSON.parse(kind.response).name;
+									vehicles.push(race);
+									if (vehicles.length === values[i].length) {
+										let row= "";
+										vehicles.forEach(elem => row+= elem + " / ");
+										row = row.slice(0,-2);
+										li.textContent = `${keys[i]}: ${row}`;
+									}
+								}
+							}
+						} else if (i === 12) {
+							if (values[i].length === 0) {
+								li.textContent = `${keys[i]}: n/a`;
+								list.appendChild(li);
+								continue;
+							}
+							let starships = [];
+							for (let j = 0; j < values[12].length; j++) {
+								const species = new XMLHttpRequest();
+								species.open('GET', `${values[12][j]}`);
+								species.send();
+								species.onload = () => {
+									let type = JSON.parse(species.response).name;
+									starships.push(type);
+									if (starships.length === values[i].length) {
+										let row= "";
+										starships.forEach(elem => row+= elem + " / ");
+										row = row.slice(0,-2);
+										li.textContent = `${keys[i]}: ${row}`;
+									}
+								}
+							}
+						} else {
+							li.textContent = `${keys[i]}: ${values[i]}`;
+						}
 						list.appendChild(li);
-						// makeRow(keys[i], values[i]);
 					}
+
 					filmsButton.addEventListener('click', () => {
+						filmsButton.disabled = true;
 						let movies = values[9];
 
 						for (let i = 0; i < movies.length; i++) {
@@ -86,10 +138,9 @@ search.addEventListener('click', () => {
 							filmTitle.send();
 
 							filmTitle.onload = () => {
-								let title = JSON.parse(filmTitle.response).title;
-								console.log(title);
+								let answer = JSON.parse(filmTitle.response)
 								let li = document.createElement('li');
-								li.textContent = `${title}`;
+								li.textContent = `Episode ${answer.episode_id}: ${answer.title} (${answer.release_date})`;
 								moviesList.appendChild(li);
 							}
 						}
@@ -99,8 +150,3 @@ search.addEventListener('click', () => {
 		}
 	}
 });
-
-// filmsButton.addEventListener('click', () => {
-// console.log(values[9]);
-// });
-
